47a8d437b7153b701966b626678663e61736d243
SammKun <85256700+SammKun@users.noreply.github.com>
2021-09-18 13:39:20 -0300

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -16,39 +16,9 @@ if (process.env.PASS) {
 }
 fs.writeFileSync("./application.yml", application)
 
-function startLavalink() {
-    const spawn = require("child_process").spawn;
-    const child = spawn("java", ["-jar", "Lavalink.jar"])
 
-    child.stdout.setEncoding("utf8")
-    child.stderr.setEncoding("utf8")
+execSync("java -jar Lavalink.jar", { stdio: "inherit" });
 
-    child.stdout.on("data", (data) => {
-        console.log(data);
-    });
-
-    child.stderr.on("data", (data) => {
-        console.error(data);
-    });
-
-    child.on("error", (error) => {
-        console.error(error);
-    });
-
-    child.on("close", (code) => {
-        console.log(`Lavalink exited with code ${code}`);
-    });
-}
-
-const url = "https://download1501.mediafire.com/vvddcune4eng/32tjs66nz40qdqk/Lavalink.jar";
-
-const start = () => {
-    const download = stream(url).pipe(createWriteStream('Lavalink.jar'));
-    download.on("finish", () => {
-        execSync("java -jar Lavalink.jar", { stdio: "inherit" });
-    });
-};
-start();
 
 
 
